2eba376f9e3baadadc0a0cb534147d1c67117dc3
devsxgui <devsxgui@outlook.com>
2023-06-12 22:26:42 -0300

    create routes modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'category', component:CategoryComponent},
+  {path:'customer', component:CustomerComponent},
+  {path:'product', component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
